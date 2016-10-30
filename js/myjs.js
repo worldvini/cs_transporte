92b72e30cc0dddbc3478cf491f248d5b3d4d19b7
Vinicius da Silva Fleury <worldvini@hotmail.com>
2016-10-30 04:18:54 -0200

    'scroll_suave'

--- js/myjs.js
+++ js/myjs.js
@@ -1,18 +1,38 @@
 
 $(document).ready(function(){
+	//carousel
 	$('.carousel').carousel({
 		interval: 3500,
 		keyboard: true
-	});
-
+	}); 
+	//menu mobile bootstrap fechando ao click de um link
 	$(function () {
         $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
         	$('.navbar-toggle:visible').click();
         });
 	});
-
-	$('.scroll').click(function(event){
+	//rolagem suave
+	
+	/*$('.scroll').click(function(event){
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});*/
+
+	
+	function scrollSuave(element, speed){
+		var href = element.attr('href');
+		var top = $(href).offset().top;
+
+		$("html , body").animate({scrollTop : top}, speed);
+	}
+
+	$(function(){
+		$("#scroll a").click(function(e){
+			e.preventDefault();
+
+			scrollSuave($(this), 800);
+		});
 	});
+
+
 });
